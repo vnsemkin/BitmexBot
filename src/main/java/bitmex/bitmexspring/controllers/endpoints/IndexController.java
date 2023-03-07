75bc52acf4e697012bf44d255f4e9b5ac97ceeeb
vnsemkin@gmail.com <vnsemkin@gmail.com>
2023-03-07 12:10:54 +0200

    Commit 3.

--- src/main/java/bitmex/bitmexspring/controllers/endpoints/IndexController.java
+++ src/main/java/bitmex/bitmexspring/controllers/endpoints/IndexController.java
@@ -56,15 +56,18 @@ public class IndexController {
             botList = botService.call(clientData);
 //            botList = botService.startNewBot(clientData);
             botList.forEach(System.out::println);
-            model.addAllAttributes(Map.of(
-                    "key", clientData.getKey(),
-                    "secret", clientData.getSecret(),
-                    "userName", clientData.getUserName(),
-                    "userEmail", clientData.getUserEmail(),
-                    "userAccount", clientData.getUserAccount(),
-                    "userCurrency", clientData.getUserCurrency(),
-                    "lastSellPrice", clientData.getLastSell(),
-                    "lastBuyPrice", clientData.getLastBuy()));
+
+            model.addAttribute("botList", botList);
+//            botList.get(0).getClientData().getKey()
+//            model.addAllAttributes(Map.of(
+//                    "key", clientData.getKey(),
+//                    "secret", clientData.getSecret(),
+//                    "userName", clientData.getUserName(),
+//                    "userEmail", clientData.getUserEmail(),
+//                    "userAccount", clientData.getUserAccount(),
+//                    "userCurrency", clientData.getUserCurrency(),
+//                    "lastSellPrice", clientData.getLastSell(),
+//                    "lastBuyPrice", clientData.getLastBuy()));
 
             /*
             Starting Web Socket
